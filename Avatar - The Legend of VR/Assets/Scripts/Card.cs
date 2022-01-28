f3f001ba80bcac167aab512d858172728fbb4718
Nicolai Hermann <nihermann@uos.de>
2022-01-28 15:46:18 +0100

    Added Dogs scene

--- Avatar - The Legend of VR/Assets/Scripts/Card.cs
+++ Avatar - The Legend of VR/Assets/Scripts/Card.cs
@@ -15,7 +15,6 @@ public class Card : MonoBehaviour
 
     public void highlight(){
         Outline outline; 
-        Debug.Log("In highlight");
         outline = this.gameObject.GetComponent<Outline>();
         outline.OutlineMode = Outline.Mode.OutlineAndSilhouette;
         outline.OutlineColor = new Color(0.78f, 0.41f, 0.39f);
---
+++ Avatar - The Legend of VR/Assets/Scripts/Field.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+    public Vector3 position;
+    
+    public Field prevField; 
+    public Field nextField; 
+
+    public Player occupiedBy = null;
+
+    public Material defaultMaterial;
+    
+    void Start(){
+         position = transform.position;
+         defaultMaterial = this.GetComponent<Renderer>().material;
+     }
+}
