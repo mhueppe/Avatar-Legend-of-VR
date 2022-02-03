4ad00dab817bbe12eeece1571deae3b43035bd54
Nicolai Hermann <nihermann@uos.de>
2022-02-03 11:27:44 +0100

    Refactored a lot of names and moved functionality from LeftHand to HandCards (recently: CardPositioner) * Fixed thumb movement animations. * renamed variables and classes and added more comments for better structure. * ExperimentManager -> TrialManager: TrialManager will control one trial and the experimentcontroller will reinit the scene every trial.

---
+++ Avatar - The Legend of VR/Assets/Scripts/Experiment/Data/TrialSetupScriptableObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/CardInformation")]
+public class TrialSetupScriptableObject : ScriptableObject
+{
+    public AvatarSetupInfos avatarSetupInfos;
+    public CardSetupInfos cardSetupInfos;
+}
+
---
+++ Avatar - The Legend of VR/Assets/Scripts/Experiment/TrialManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using VRAvatar;
+
+public class TrialManager : MonoBehaviour
+{
+    private readonly StateMachine _stateMachine = new();
+    
+    [SerializeField] private VRPlayer player;
+    
+    private TrialInfo _currentTrial;
+
+    public void Awake()
+    {
+        _currentTrial = ExperimentController.Instance.InitNewTrial();
+
+        // give him cards
+        var refillHandCards = new RefillHandCards(
+            _currentTrial.cardSetupInfos.cardStaple,
+            player,
+            _currentTrial.cardSetupInfos.numberOfHandCardsAtStart,
+            0.5f);
+
+        // see cards - preview them - select one
+        var selectCard = new PlayerSelectCardState(player, _currentTrial);
+
+        // Move VRPlayer - activate navmesh from potentially following avatars
+        var movePlayer = new MovePlayerState(player, _currentTrial);
+        
+        // if reached selection state, select/swap
+        
+        // if not reached but cards are empty, refill hand cards
+        
+        // done state
+        var done = new DoneState();
+
+
+        // refill hand cards --()--> select card
+        _stateMachine.AddTransition(refillHandCards, selectCard, () => refillHandCards.AreRefilled);
+        // select card --()--> move player
+        _stateMachine.AddTransition(selectCard, movePlayer, () => selectCard.CardSelected);
+        
+        
+        // move player --(if enough handcards)--> select card
+        _stateMachine.AddTransition(movePlayer, selectCard, () => movePlayer.FinishedMovement && player.leftHand.handCards.NumberOfCards > 0);
+        // move player --(if no more cards on hand)--> refill cards
+        _stateMachine.AddTransition(movePlayer, refillHandCards, () => movePlayer.FinishedMovement && player.leftHand.handCards.NumberOfCards == 0);
+        // move player --(if goal reached)--> save trial
+        _stateMachine.AddTransition(movePlayer, done, () => movePlayer.ReachedGoalField);
+        
+        
+        // Start state
+        _stateMachine.SetState(refillHandCards);
+    }
+
+    private void Start()
+    {
+        // Other setups which are not related to state machine
+    }
+
+    private void Update() => _stateMachine.Tick();
+    
+}
+
